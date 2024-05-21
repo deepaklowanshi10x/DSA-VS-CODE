7eb808c7876f66544701fef87e22cb66bdb74298
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2024-05-21 23:43:36 +0530

    DSA Q PRACTICE

--- Prectice Questions/example9.cpp
+++ Prectice Questions/example9.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int fibbo(int n)
+int fibonnaci(int n)
 {
     int a = 0, b = 1, ans = 0;
     for (int i = 2; i <= n - 1; i++)
@@ -18,6 +18,11 @@ int main()
     int n;
     cout << "enter last term of fibonacci series = ";
     cin >> n;
-    cout << "last term of fibonacci series is = " << fibbo(n);
+    cout << "last term of fibonacci series is = " << fibonnaci(n);
     return 0;
 }
+
+// output :--
+
+// enter last term of fibonacci series = 5
+// last term of fibonacci series is = 3
